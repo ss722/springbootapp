6703b969048e2e932ca09cb8448fd36453611ac8
ss722 <psaurabh722@gmail.com>
2025-01-06 21:45:15 +0530

    COMMIT

---
+++ src/main/java/com/ecommerce/project/SsEcomApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsEcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsEcomApplication.class, args);
+	}
+
+}
